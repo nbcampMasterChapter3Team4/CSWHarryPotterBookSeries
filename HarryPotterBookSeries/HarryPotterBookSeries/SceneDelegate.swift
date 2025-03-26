48292d00fe074b9a3bf3aad6459d3128f4cb3bfc
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-26 16:49:21 +0900

    [Setting] #1 - remove StoryBoard

---
+++ HarryPotterBookSeries/HarryPotterBookSeries/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  HarryPotterBookSeries
+//
+//  Created by 천성우 on 3/26/25.
+//
+
+import UIKit
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
---
+++ HarryPotterBookSeries/HarryPotterBookSeries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterBookSeries
+//
+//  Created by 천성우 on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
