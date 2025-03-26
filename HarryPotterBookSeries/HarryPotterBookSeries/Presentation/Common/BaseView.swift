bdb7ecced51dc6818325283f3249bd9e5c55d998
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-26 18:57:01 +0900

    [Setting] #1 - add BaseView & BaseViewController

---
+++ HarryPotterBookSeries/HarryPotterBookSeries/Presentation/Common/BaseView.swift
@@ -0,0 +1,41 @@
+//
+//  BaseView.swift
+//  HarryPotterBookSeries
+//
+//  Created by 천성우 on 3/26/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+class BaseView: UIView {
+    
+    private lazy var viewName = self.className
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bindViewModel()
+        setStyles()
+        setLayout()
+    }
+    
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+        print("🧵 \(viewName) has been successfully Removed")
+    }
+    
+    /// Data 와 UI 를 bind 합니다.
+    func bindViewModel() {}
+    /// View 의 Style 을 set 합니다.
+    func setStyles() {}
+    /// View 의 Layout 을 set 합니다.
+    func setLayout() {}
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
