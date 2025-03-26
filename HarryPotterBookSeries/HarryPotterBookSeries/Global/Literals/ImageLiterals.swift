f18637a17b688a8d22475958644f3ff972f29848
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-26 18:55:06 +0900

    [Setting] #1 - add Literals

---
+++ HarryPotterBookSeries/HarryPotterBookSeries/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLiterals.swift
+//  sparta
+//
+//  Created by 천성우 on 3/6/25.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    static var harrypotter_poster_one: UIImage { .load(name: "harrypotter1").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_two: UIImage { .load(name: "harrypotter2").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_three: UIImage { .load(name: "harrypotter3").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_four: UIImage { .load(name: "harrypotter4").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_five: UIImage { .load(name: "harrypotter5").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_six: UIImage { .load(name: "harrypotter6").withRenderingMode(.alwaysOriginal)}
+    static var harrypotter_poster_seven: UIImage { .load(name: "harrypotter7").withRenderingMode(.alwaysOriginal)}
+}
