fe4d9b5099c81dd7e181cf632734efc47b74c759
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-27 20:24:21 +0900

    [Feat] #2 -책 제목을 표시하는 TitleLabelView를 구현합니다.

---
+++ HarryPotterBookSeries/HarryPotterBookSeries/Presentation/Main/View/TitleLabelView.swift
@@ -0,0 +1,47 @@
+//
+//  TitleLabelView.swift
+//  HarryPotterBookSeries
+//
+//  Created by 천성우 on 3/27/25.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class TitleLabelView: BaseView {
+    
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel()
+    
+    
+    override func setStyles() {
+        titleLabel.do {
+            $0.textColor = UIColor(hex: "#000000")
+            $0.font = UIFont.boldSystemFont(ofSize: 24)
+            $0.textAlignment = .center
+            $0.numberOfLines = 2
+//            $0.text = "Harry Potter and the Philosopher's Stone"
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    override func setLayout() {
+        addSubviews(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func getTitleLabel() -> UILabel {
+        return titleLabel
+    }
+}
