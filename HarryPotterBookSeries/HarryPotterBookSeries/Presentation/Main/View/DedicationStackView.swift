e1e27a7917ec5bd9c25bd9b7135b63c159d95d33
cjs1399 <cjs1399@naver.com>
2025-03-28 15:37:40 +0900

    [Feat] #4 - SummaryView와 DedicationView를 분리하고 textView 표시를 위한 설정을 합니다.

---
+++ HarryPotterBookSeries/HarryPotterBookSeries/Presentation/Main/View/DedicationStackView.swift
@@ -0,0 +1,67 @@
+//
+//  DedicationStackView.swift
+//  HarryPotterBookSeries
+//
+//  Created by 천성우 on 3/28/25.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class DedicationStackView: BaseView {
+    
+    // MARK: - UI Components
+    
+    private let dedicationLabel = UILabel()
+    private let dedicationTextView = UITextView()
+    private let dedicationStackView = UIStackView()
+
+    
+    override func setStyles() {
+        dedicationLabel.do {
+            $0.text = "Dedication"
+            $0.textColor = UIColor(hex: "#000000")
+            $0.font = .boldSystemFont(ofSize: 18)
+        }
+        
+        dedicationTextView.do {
+            $0.text = "For Jessica, who loves stories, for Anne, who loved them too, and for Di, who heard this one first"
+            $0.font = .systemFont(ofSize: 14)
+            $0.textColor = UIColor(hex: "#555555") // UIColor.darkGray 색상과 일치하는 hex code 입니다.
+            $0.isScrollEnabled = false
+            $0.textContainerInset = .zero
+            $0.textContainer.lineFragmentPadding = 0
+        }
+        
+        dedicationStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 8
+            $0.alignment = .leading
+        }
+        
+
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    override func setLayout() {
+        addSubviews(dedicationStackView)
+        dedicationStackView.addArrangedSubviews(dedicationLabel, dedicationTextView)
+
+        
+        dedicationStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        dedicationTextView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Methods
+    
+
+}
--- HarryPotterBookSeries/HarryPotterBookSeries/Presentation/Main/View/SummaryStackView.swift
+++ HarryPotterBookSeries/HarryPotterBookSeries/Presentation/Main/View/SummaryStackView.swift
@@ -14,33 +14,11 @@ class SummaryStackView: BaseView {
     
     // MARK: - UI Components
     
-    private let dedicationLabel = UILabel()
-    private let dedicationTextView = UITextView()
-    private let dedicationStackView = UIStackView()
     private let summaryLabel = UILabel()
     private let summaryTextView = UITextView()
     private let summaryStackView = UIStackView()
     
-    
-    
     override func setStyles() {
-        dedicationLabel.do {
-            $0.text = "Dedication"
-            $0.textColor = UIColor(hex: "#000000")
-            $0.font = .boldSystemFont(ofSize: 18)
-        }
-        
-        dedicationTextView.do {
-            $0.font = .systemFont(ofSize: 14)
-            $0.textColor = UIColor(hex: "#555555") // UIColor.darkGray 색상과 일치하는 hex code 입니다.
-        }
-        
-        dedicationStackView.do {
-            $0.axis = .vertical
-            $0.spacing = 10
-            $0.alignment = .leading
-        }
-        
         summaryLabel.do {
             $0.text = "Summary"
             $0.textColor = UIColor(hex: "#000000")
@@ -48,14 +26,18 @@ class SummaryStackView: BaseView {
         }
         
         summaryTextView.do {
+            $0.text = "Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry's eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin!"
             $0.font = .systemFont(ofSize: 14)
             $0.textColor = UIColor(hex: "#555555")
+            $0.isScrollEnabled = false
+            $0.textContainerInset = .zero
+            $0.textContainer.lineFragmentPadding = 0
         }
         
         summaryStackView.do {
             $0.axis = .vertical
-            $0.spacing = 10
-            $0.alignment = .leading
+            $0.spacing = 8
+            $0.alignment = .fill
         }
         
     }
@@ -63,6 +45,16 @@ class SummaryStackView: BaseView {
     // MARK: - Layout Helper
     
     override func setLayout() {
+        addSubviews(summaryStackView)
+        summaryStackView.addArrangedSubviews(summaryLabel, summaryTextView)
+
+        summaryStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    
+        summaryTextView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+        }
 
     }
     
